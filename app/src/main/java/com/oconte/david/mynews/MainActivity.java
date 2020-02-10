a7ac9aeea4083cf6da3bc00dead290c7bb645f89
David.OA <david.oconte@orange.fr>
2020-02-10 14:39:10 +0100

    I add the Option menu.

--- app/src/main/java/com/oconte/david/mynews/MainActivity.java
+++ app/src/main/java/com/oconte/david/mynews/MainActivity.java
@@ -1,8 +1,15 @@
 package com.oconte.david.mynews;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.oconte.david.mynews.OptionMenu.AboutActivity;
+import com.oconte.david.mynews.OptionMenu.HelpActivity;
+
 import butterknife.ButterKnife;
 import butterknife.BindView;
 
@@ -44,5 +51,53 @@ public class MainActivity extends AppCompatActivity {
         //afficher le bouton retour
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * For the right menu
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //View view = toolbar.findViewById(id);
+        switch (id) {
+            case R.id.menu_main_activity_notifications:
+                this.startNotificationsActivity();
+                return true;
+            case R.id.menu_main_activity_help:
+                this.startHelpActivity();
+                return true;
+            case R.id.menu_main_activity_about:
+                this.startAboutActivity();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void startNotificationsActivity(){
+        Intent intent = new Intent(this, NotificationsActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void startHelpActivity(){
+        Intent intent = new Intent(this, HelpActivity.class);
+        startActivity(intent);
+    }
+
+    private void startAboutActivity(){
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 }
