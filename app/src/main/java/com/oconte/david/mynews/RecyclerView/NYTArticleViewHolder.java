1b88b2fdae458a727d28003bc1c31994d7cb3fc0
David.OA <david.oconte@orange.fr>
2020-02-10 15:32:46 +0100

    I add differents ressources and make disegn for recyclerview.

--- app/src/main/java/com/oconte/david/mynews/RecyclerView/NYTArticleViewHolder.java
+++ app/src/main/java/com/oconte/david/mynews/RecyclerView/NYTArticleViewHolder.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.oconte.david.mynews.ConfigureDate;
 import com.oconte.david.mynews.Models.Article;
 import com.oconte.david.mynews.R;
 import com.squareup.picasso.Picasso;
@@ -15,6 +16,10 @@ import butterknife.ButterKnife;
 public class NYTArticleViewHolder extends RecyclerView.ViewHolder {
 
     @BindView(R.id.fragment_main_title) TextView textView;
+    @BindView(R.id.fragment_main_section) TextView textView1;
+    @BindView(R.id.fragment_main_subsection) TextView textView2;
+    @BindView(R.id.fragment_main_date) TextView date;
+
     @BindView(R.id.fragment_main_image) ImageView imageView;
 
     public NYTArticleViewHolder(View itemView) {
@@ -23,12 +28,28 @@ public class NYTArticleViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void updateWithNYTArticle (Article article) {
-        this.textView.setText(article.getAbstract());
-        Picasso.get().load(getFirstUrl(article)).into(this.imageView);
+        this.textView.setText(article.getTitle());
+        this.textView1.setText(article.getSection());
+        this.textView2.setText(article.getSubsection());
+        this.date.setText(ConfigureDate.convertDateFromAPIToDisplay(article.getPublishedDate()));
+
+        Picasso.get()
+                .load(getFirstUrl(article))
+                .resize(60, 60)
+                .into(this.imageView);
     }
 
+    /**
+     * It's for get url Images
+     * @param article
+     * @return
+     */
     public String getFirstUrl(Article article) {
-        String url = article.getUrl();
-        return url;
+        if (article.getMultimedia() != null && article.getMultimedia().size() > 0) {
+            String url = article.getMultimedia().get(0).getUrl();
+            return url;
+        }
+
+        return null;
     }
 }
