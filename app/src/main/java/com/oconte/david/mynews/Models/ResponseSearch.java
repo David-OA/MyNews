d96c49ca696a4d21b274946bf6a23281b4c3ec5b
David.OA <david.oconte@orange.fr>
2020-03-03 10:09:00 +0100

    I add some Models.

---
+++ app/src/main/java/com/oconte/david/mynews/Models/ResponseSearch.java
@@ -0,0 +1,32 @@
+package com.oconte.david.mynews.Models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class ResponseSearch {
+
+    @SerializedName("docs")
+    @Expose
+    private List<ArticleSearch> docs = null;
+    @SerializedName("meta")
+    @Expose
+    private MetaSearch meta;
+
+    public List<ArticleSearch> getDocs() {
+        return docs;
+    }
+
+    public void setDocs(List<ArticleSearch> docs) {
+        this.docs = docs;
+    }
+
+    public MetaSearch getMeta() {
+        return meta;
+    }
+
+    public void setMeta(MetaSearch meta) {
+        this.meta = meta;
+    }
+}
